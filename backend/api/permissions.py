a8b34d54938610577cf327404acd7fcfec144a1f
Yo_Moe <yo_moe@icloud.com>
2023-06-29 17:42:12 +0800

    add demo data, fix bugs

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,10 +2,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthorOrAdminOrReadOnly(BasePermission):
-
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in SAFE_METHODS
-                or request.user.is_authenticated
+                request.method in SAFE_METHODS or request.user.is_authenticated
                 and (request.user == obj.author or request.user.is_staff)
         )
